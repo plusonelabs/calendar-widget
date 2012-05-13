9f2c69706167d5881bf31bb0810314beb59bef5e
Moritz Post <moritzpost@gmail.com>
2012-05-13 18:08:28 +0200

    Removed deprecated files from project renaming.

--- app/com.moritzpost.calendar/gen/com/moritzpost/calendar/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.moritzpost.calendar;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- app/com.moritzpost.calendar/src/com/moritzpost/calendar/model/DayHeader.java
+++
@@ -1,9 +0,0 @@
-package com.moritzpost.calendar.model;
-
-public class DayHeader extends CalenderEntry {
-
-	public DayHeader(long date) {
-		setStartDate(date);
-	}
-
-}

611b3dec9859127fbf321d999a465893f6853215
Moritz Post <moritzpost@gmail.com>
2012-07-08 12:32:46 +0200

    #14 Added support for am/pm format.

--- app/com.plusonelabs.calendar/src/com/plusonelabs/calendar/prefs/ICalendarPreferences.java
+++ app/com.plusonelabs.calendar/src/com/plusonelabs/calendar/prefs/ICalendarPreferences.java
@@ -10,5 +10,7 @@ public interface ICalendarPreferences {
 	public static final String PREF_TEXT_SIZE = "textSize";
 	public static final String PREF_INDICATE_RECURRING = "indicateRecurring";
 	public static final String PREF_INDICATE_ALERTS = "indicateAlerts";
+	public static final String PREF_DATE_FORMAT = "dateFormat";
+	public static final String PREF_DATE_FORMAT_DEFAULT = "auto";
 
 }

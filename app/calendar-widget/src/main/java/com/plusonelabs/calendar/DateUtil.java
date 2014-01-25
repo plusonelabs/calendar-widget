f865ed830098964557f8b54e9b28aa30601d3f49
Moritz Post <moritzpost@gmail.com>
2014-01-25 13:07:03 +0100

    Fixes #99. "Today" and "Tomorrow" date headers are properly shown again.

--- app/calendar-widget/src/main/java/com/plusonelabs/calendar/DateUtil.java
+++ app/calendar-widget/src/main/java/com/plusonelabs/calendar/DateUtil.java
@@ -12,31 +12,32 @@ import java.util.Locale;
 
 public class DateUtil {
 
-	private static final String TWELVE = "12";
-	private static final String NOON_AM = "12:00 AM";
-	private static final String EMPTY_STRING = "";
+    private static final String TWELVE = "12";
+    private static final String NOON_AM = "12:00 AM";
+    private static final String EMPTY_STRING = "";
     private static final String COMMA_SPACE = ", ";
 
-	public static boolean isMidnight(DateTime date) {
-		return date.isEqual(date.withTimeAtStartOfDay());
-	}
+    public static boolean isMidnight(DateTime date) {
+        return date.isEqual(date.withTimeAtStartOfDay());
+    }
 
-	public static boolean hasAmPmClock(Locale locale) {
-		DateFormat stdFormat = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.US);
-		DateFormat localeFormat = DateFormat.getTimeInstance(DateFormat.LONG, locale);
-		String midnight = EMPTY_STRING;
-		try {
-			midnight = localeFormat.format(stdFormat.parse(NOON_AM));
-		} catch (ParseException ignore) {
-			// we ignore this exception deliberately
-		}
-		return midnight.contains(TWELVE);
-	}
+    public static boolean hasAmPmClock(Locale locale) {
+        DateFormat stdFormat = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.US);
+        DateFormat localeFormat = DateFormat.getTimeInstance(DateFormat.LONG, locale);
+        String midnight = EMPTY_STRING;
+        try {
+            midnight = localeFormat.format(stdFormat.parse(NOON_AM));
+        } catch (ParseException ignore) {
+            // we ignore this exception deliberately
+        }
+        return midnight.contains(TWELVE);
+    }
 
     public static String createDateString(Context context, DateTime dateTime) {
-        if (dateTime.withTimeAtStartOfDay().isEqual(DateTime.now())) {
+        DateTime timeAtStartOfToday = DateTime.now().withTimeAtStartOfDay();
+        if (dateTime.withTimeAtStartOfDay().isEqual(timeAtStartOfToday)) {
             return createDateString(context, dateTime.toDate(), context.getString(R.string.today));
-        } else if (dateTime.withTimeAtStartOfDay().isEqual(DateTime.now().plusDays(1))) {
+        } else if (dateTime.withTimeAtStartOfDay().isEqual(timeAtStartOfToday.plusDays(1))) {
             return createDateString(context, dateTime.toDate(), context.getString(R.string.tomorrow));
         }
         return DateUtils.formatDateTime(context, dateTime.toDate().getTime(),

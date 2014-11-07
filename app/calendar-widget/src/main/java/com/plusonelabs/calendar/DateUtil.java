c1499813ead92f0f090f44f937bbb3ebf8dd8f53
Exe Curia <sequielo@users.noreply.github.com>
2014-11-07 00:17:44 -0300

    Show number of days for future events

--- app/calendar-widget/src/main/java/com/plusonelabs/calendar/DateUtil.java
+++ app/calendar-widget/src/main/java/com/plusonelabs/calendar/DateUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.text.format.DateUtils;
 
 import org.joda.time.DateTime;
+import org.joda.time.Days;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -16,6 +17,8 @@ public class DateUtil {
     private static final String NOON_AM = "12:00 AM";
     private static final String EMPTY_STRING = "";
     private static final String COMMA_SPACE = ", ";
+    private static final String PRE_DAYS = " (+";
+    private static final String POST_DAYS = ")";
 
     public static boolean isMidnight(DateTime date) {
         return date.isEqual(date.withTimeAtStartOfDay());
@@ -41,10 +44,14 @@ public class DateUtil {
             return createDateString(context, dateTime.toDate(), context.getString(R.string.tomorrow));
         }
         return DateUtils.formatDateTime(context, dateTime.toDate().getTime(),
-                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_WEEKDAY);
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_WEEKDAY) + PRE_DAYS + daysBetween(DateTime.now().toDate(), dateTime.toDate()) + POST_DAYS;
     }
 
     private static String createDateString(Context context, Date date, String prefix) {
         return prefix + COMMA_SPACE + DateUtils.formatDateTime(context, date.getTime(), DateUtils.FORMAT_SHOW_DATE);
+    }
+    
+    private static String daysBetween(Date start, Date end) {
+        return String.valueOf( Days.daysBetween( start.toDateMidnight(), end.toDateMidnight() ).getDays() );
     }
 }

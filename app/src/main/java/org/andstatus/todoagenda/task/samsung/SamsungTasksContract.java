0f8fc797a05920daaebbc8a9ae06469f1b8a0273
Eduardo M KALINOWSKI <eduardo@kalinowski.com.br>
2019-07-21 11:08:07 -0300

    Adds task list support for Samsung Tasks (with limitations).
    - Even though there are columns for a "display name" and
      "account name" in the provider, both always seem to return the
      same text, so the "display name" has been fixed to "Tasks".
    - Lists from synced accounts don't have a color set, so a fixed color
      is used for each task list. They will repeat if the user has many
      task lists.

--- app/src/main/java/org/andstatus/todoagenda/task/samsung/SamsungTasksContract.java
+++ app/src/main/java/org/andstatus/todoagenda/task/samsung/SamsungTasksContract.java
@@ -4,13 +4,27 @@ import android.net.Uri;
 
 public class SamsungTasksContract {
 
-    public static final Uri PROVIDER_URI = Uri.parse("content://com.android.calendar/syncTasks");
+    public static class Tasks {
 
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_TITLE = "subject";
-    public static final String COLUMN_DUE_DATE = "utc_due_date";
-    public static final String COLUMN_COMPLETE = "complete";
-    public static final String COLUMN_DELETED = "deleted";
+        public static final Uri PROVIDER_URI = Uri.parse("content://com.android.calendar/syncTasks");
+
+        public static final String COLUMN_ID = "_id";
+        public static final String COLUMN_TITLE = "subject";
+        public static final String COLUMN_DUE_DATE = "utc_due_date";
+        public static final String COLUMN_COLOR = "secAccountColor";
+        public static final String COLUMN_COMPLETE = "complete";
+        public static final String COLUMN_DELETED = "deleted";
+        public static final String COLUMN_LIST_ID = "accountKey";
+    }
+
+    public static class TaskLists {
+
+        public static final Uri PROVIDER_URI = Uri.parse("content://com.android.calendar/TasksAccounts");
+
+        public static final String COLUMN_ID = "_sync_account_key";
+        public static final String COLUMN_NAME = "displayName";
+        public static final String COLUMN_COLOR = "secAccountColor";
+    }
 
     public static final String INTENT_EXTRA_TASK = "task";
     public static final String INTENT_EXTRA_SELECTED = "selected";
--- app/src/main/java/org/andstatus/todoagenda/task/samsung/SamsungTasksProvider.java
+++ app/src/main/java/org/andstatus/todoagenda/task/samsung/SamsungTasksProvider.java
@@ -3,7 +3,9 @@ package org.andstatus.todoagenda.task.samsung;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.text.TextUtils;
 
+import org.andstatus.todoagenda.R;
 import org.andstatus.todoagenda.calendar.CalendarQueryResult;
 import org.andstatus.todoagenda.calendar.CalendarQueryResultsStorage;
 import org.andstatus.todoagenda.prefs.EventSource;
@@ -13,6 +15,7 @@ import org.andstatus.todoagenda.task.TaskEvent;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public class SamsungTasksProvider extends AbstractTaskProvider {
 
@@ -27,11 +30,13 @@ public class SamsungTasksProvider extends AbstractTaskProvider {
     }
 
     private List<TaskEvent> queryTasks() {
-        Uri uri = SamsungTasksContract.PROVIDER_URI;
+        Uri uri = SamsungTasksContract.Tasks.PROVIDER_URI;
         String[] projection = {
-                SamsungTasksContract.COLUMN_ID,
-                SamsungTasksContract.COLUMN_TITLE,
-                SamsungTasksContract.COLUMN_DUE_DATE
+                SamsungTasksContract.Tasks.COLUMN_ID,
+                SamsungTasksContract.Tasks.COLUMN_TITLE,
+                SamsungTasksContract.Tasks.COLUMN_DUE_DATE,
+                SamsungTasksContract.Tasks.COLUMN_COLOR,
+                SamsungTasksContract.Tasks.COLUMN_LIST_ID,
         };
         String where = getWhereClause();
 
@@ -65,34 +70,84 @@ public class SamsungTasksProvider extends AbstractTaskProvider {
 
     private String getWhereClause() {
         StringBuilder whereBuilder = new StringBuilder();
-        whereBuilder.append(SamsungTasksContract.COLUMN_COMPLETE).append(EQUALS).append("0");
-        whereBuilder.append(AND).append(SamsungTasksContract.COLUMN_DELETED).append(EQUALS).append("0");
+        whereBuilder.append(SamsungTasksContract.Tasks.COLUMN_COMPLETE).append(EQUALS).append("0");
+        whereBuilder.append(AND).append(SamsungTasksContract.Tasks.COLUMN_DELETED).append(EQUALS).append("0");
 
         whereBuilder.append(AND_BRACKET)
-                .append(SamsungTasksContract.COLUMN_DUE_DATE).append(LTE).append(mEndOfTimeRange.getMillis())
+                .append(SamsungTasksContract.Tasks.COLUMN_DUE_DATE).append(LTE).append(mEndOfTimeRange.getMillis())
                 .append(OR)
-                .append(SamsungTasksContract.COLUMN_DUE_DATE).append(IS_NULL)
+                .append(SamsungTasksContract.Tasks.COLUMN_DUE_DATE).append(IS_NULL)
                 .append(CLOSING_BRACKET);
+
+        Set<String> taskLists = getSettings().getActiveTaskLists();
+        if (!taskLists.isEmpty()) {
+            whereBuilder.append(AND);
+            whereBuilder.append(SamsungTasksContract.Tasks.COLUMN_LIST_ID);
+            whereBuilder.append(" IN ( ");
+            whereBuilder.append(TextUtils.join(",", taskLists));
+            whereBuilder.append(CLOSING_BRACKET);
+        }
 
         return whereBuilder.toString();
     }
 
     private TaskEvent createTask(Cursor cursor) {
         TaskEvent task = new SamsungTaskEvent(zone);
-        task.setId(cursor.getLong(cursor.getColumnIndex(SamsungTasksContract.COLUMN_ID)));
-        task.setTitle(cursor.getString(cursor.getColumnIndex(SamsungTasksContract.COLUMN_TITLE)));
+        task.setId(cursor.getLong(cursor.getColumnIndex(SamsungTasksContract.Tasks.COLUMN_ID)));
+        task.setTitle(cursor.getString(cursor.getColumnIndex(SamsungTasksContract.Tasks.COLUMN_TITLE)));
 
-        int dueDateIdx = cursor.getColumnIndex(SamsungTasksContract.COLUMN_DUE_DATE);
+        int dueDateIdx = cursor.getColumnIndex(SamsungTasksContract.Tasks.COLUMN_DUE_DATE);
         Long dueMillis = null;
         if (!cursor.isNull(dueDateIdx)) {
             dueMillis = cursor.getLong(dueDateIdx);
         }
         task.setDates(null, dueMillis);
+
+        task.setColor(getColor(cursor, cursor.getColumnIndex(SamsungTasksContract.Tasks.COLUMN_COLOR),
+                cursor.getInt(cursor.getColumnIndex(SamsungTasksContract.Tasks.COLUMN_LIST_ID))));
+
         return task;
     }
 
     @Override
     public Collection<EventSource> getTaskLists() {
-        return new ArrayList<>();
+        ArrayList<EventSource> eventSources = new ArrayList<>();
+
+        String[] projection = {
+                SamsungTasksContract.TaskLists.COLUMN_ID,
+                SamsungTasksContract.TaskLists.COLUMN_NAME,
+                SamsungTasksContract.TaskLists.COLUMN_COLOR,
+        };
+        Cursor cursor = context.getContentResolver().query(SamsungTasksContract.TaskLists.PROVIDER_URI, projection, null, null, null);
+        if (cursor == null) {
+            return eventSources;
+        }
+
+        String taskListName = context.getResources().getString(R.string.task_prefs);
+        int idIdx = cursor.getColumnIndex(SamsungTasksContract.TaskLists.COLUMN_ID);
+        int nameIdx = cursor.getColumnIndex(SamsungTasksContract.TaskLists.COLUMN_NAME);
+        int colorIdx = cursor.getColumnIndex(SamsungTasksContract.TaskLists.COLUMN_COLOR);
+        try {
+            while (cursor.moveToNext()) {
+                int id = cursor.getInt(idIdx);
+                EventSource eventSource = new EventSource(id, taskListName,
+                        cursor.getString(nameIdx), getColor(cursor, colorIdx, id));
+                eventSources.add(eventSource);
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return eventSources;
+    }
+
+    private int getColor(Cursor cursor, int colorIdx, int accountId) {
+        if (!cursor.isNull(colorIdx)) {
+            return getAsOpaque(cursor.getInt(colorIdx));
+        } else {
+            int[] fixedColors = context.getResources().getIntArray(R.array.task_list_colors);
+            int arrayIdx = accountId % fixedColors.length;
+            return fixedColors[arrayIdx];
+        }
     }
 }

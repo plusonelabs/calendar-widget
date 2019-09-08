4ae37ddbf33be151815fb264d1da19e57802f244
Yuri Volkov <yvolk@yurivolkov.com>
2019-09-08 17:27:05 +0300

    #319 Add more values to "Show events that ended recently", up to one year

--- app/src/main/java/org/andstatus/todoagenda/EndedSomeTimeAgo.java
+++ app/src/main/java/org/andstatus/todoagenda/EndedSomeTimeAgo.java
@@ -4,19 +4,61 @@ import org.joda.time.DateTime;
 
 public enum EndedSomeTimeAgo {
     NONE("NONE", 0),
+    ONE_HOUR("ONE_HOUR", 1),
+    TWO_HOURS("TWO_HOURS", 2),
+    FOUR_HOURS("FOUR_HOURS", 4),
     TODAY("TODAY", 0) {
         @Override
         public DateTime endedAt(DateTime now) {
             return now.withTimeAtStartOfDay();
         }
     },
-    ONE_HOUR("ONE_HOUR", 1),
-    TWO_HOURS("TWO_HOURS", 2),
-    FOUR_HOURS("FOUR_HOURS", 4),
     YESTERDAY("YESTERDAY", 0) {
         @Override
         public DateTime endedAt(DateTime now) {
             return now.withTimeAtStartOfDay().minusDays(1);
+        }
+    },
+    ONE_WEEK("ONE_WEEK", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusDays(7);
+        }
+    },
+    TWO_WEEKS("TWO_WEEKS", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusDays(14);
+        }
+    },
+    ONE_MONTH("ONE_MONTH", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusMonths(1);
+        }
+    },
+    TWO_MONTHS("TWO_MONTHS", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusMonths(2);
+        }
+    },
+    THREE_MONTHS("THREE_MONTHS", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusMonths(3);
+        }
+    },
+    SIX_MONTHS("SIX_MONTHS", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusMonths(6);
+        }
+    },
+    ONE_YEAR("ONE_YEAR", 0) {
+        @Override
+        public DateTime endedAt(DateTime now) {
+            return now.withTimeAtStartOfDay().minusYears(1);
         }
     };
 
